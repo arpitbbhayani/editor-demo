3f9811ae696cab778e4a98c21fe401e05db7aaf8
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2016-02-05 01:36:32 +0530

    Completing file save, syntax highlight

--- app/static/js/editor.js
+++ app/static/js/editor.js
@@ -1,3 +1,60 @@
 $(document).ready(function() {
+    var editor = CodeMirror.fromTextArea(document.getElementById("online_editor"), {
+        lineNumbers: true,
+        mode: "default",
+        theme: "material"
+    });
+
+    function updatemode(mode) {
+        editor.setOption("mode", mode);
+    }
+
+    $('#open_file_form').form({
+        fields: {
+            filepath : 'empty'
+        }
+    });
+
+    $('#open_file_form_submit_button').click(function () {
+        var $form = $('#open_file_form');
+        var is_valid = $form.form('validate form');
+        if(is_valid) {
+            var $button = $(this);
+            var filepath = $('#open_file_form').find('[name="filepath"]').val();
+
+            $button.addClass('loading');
+            $.get('/fetch', {path: filepath}, function(result) {
+                $('#open_file_form_filepath').text(filepath);
+                editor.setValue(result.content);
+            }).fail(function(response) {
+                alert(response.responseText.responseText);
+            }).always(function(){
+                $button.removeClass('loading');
+            });
+        }
+    });
+
+    $('#open_file_form_update_mode_dropdown').change(function() {
+        updatemode($(this).val());
+    });
+
+    $('#open_file_form_save_button').click(function () {
+        var $button = $(this);
+        var filepath = $('#open_file_form_filepath').text();
+
+        if(confirm("This will override the file. Do you really want to continue?")) {
+            var content = editor.getValue();
+            $button.addClass('loading');
+            $.get('/save', {path: filepath, content: content}, function(result) {
+                alert(result.content);
+            }).fail(function(response) {
+                alert(response.responseText.responseText);
+            }).always(function() {
+                $button.removeClass('loading');
+            });
+        }
+    });
+
+    $('.ui.dropdown').dropdown();
 
 });
--- app/views/pages.py
+++ app/views/pages.py
@@ -1,9 +1,8 @@
 from flask import Blueprint
-
-from flask import render_template, request, redirect
-from flask import url_for, make_response
+from flask import render_template, jsonify, request
 
 mod = Blueprint('pages', __name__, )
+
 
 @mod.route('/', methods=["GET", "POST"])
 def index():
@@ -13,3 +12,37 @@ def index():
 @mod.route('/editor', methods=["GET", "POST"])
 def editor():
     return render_template('editor.html')
+
+
+@mod.route('/fetch', methods=["GET", "POST"])
+def fetch_file():
+    filepath = request.args.get('path')
+    if not filepath:
+        return jsonify(responseText="No path passed"), 403
+    try:
+        content = open(filepath, "rb").read()
+    except:
+        return jsonify(responseText="Error reading file"), 403
+
+    return jsonify(content=content)
+
+
+@mod.route('/save', methods=["GET", "POST"])
+def save_file():
+    filepath = request.args.get('path')
+    content = request.args.get('content')
+
+    if not filepath:
+        return jsonify(responseText="No path passed"), 403
+
+    if not content:
+        return jsonify(responseText="No content passed"), 403
+
+    try:
+        with open(filepath, "wb") as f:
+            f.write(content)
+            message = "File save successfully"
+    except:
+        return jsonify(responseText="Error writing file"), 403
+
+    return jsonify(content=message)
